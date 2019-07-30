0f2d750154a389d76cc5b95ba6d8b68eb29b5c9b
galaxydeve <devphs513@gmail.com>
2019-07-30 21:39:32 +0800

    initial build

---
+++ precache-manifest.3a9cf6a9ff32a7ba2e90badfddf05e94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97df875126553a084033b12bc6199363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d1be0ab15ab82c7c5c",
+    "url": "/static/css/main.e6999e84.chunk.css"
+  },
+  {
+    "revision": "af1ce02383e651351174",
+    "url": "/static/js/2.2cb0fb2f.chunk.js"
+  },
+  {
+    "revision": "16d1be0ab15ab82c7c5c",
+    "url": "/static/js/main.f49c1b3f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
