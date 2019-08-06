40a39c07fad91d009d9173a4573ed6047cfb4ca1
galaxydeve <devphs513@gmail.com>
2019-08-06 13:03:19 +0800

    fix repositioning

---
+++ precache-manifest.aaa88cf332cf322219c20e63802f7eea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecc43145c415c3ad0aa1964d4f30b3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15673b83d3237e606bec",
+    "url": "/static/css/main.e182199c.chunk.css"
+  },
+  {
+    "revision": "a69566ba005e738895cf",
+    "url": "/static/js/2.337d9324.chunk.js"
+  },
+  {
+    "revision": "15673b83d3237e606bec",
+    "url": "/static/js/main.a4932fe8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
