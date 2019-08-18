808d362af0b1b2cf3674b83e779c9658aba387c0
galaxydeve <devphs513@gmail.com>
2019-08-18 18:53:54 +0800

    update path

--- precache-manifest.0e1c5c2f115254dc55c8f9b2b7fc76ed.js
+++ precache-manifest.0e1c5c2f115254dc55c8f9b2b7fc76ed.js
@@ -21,14 +21,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "62cee232d3b161c24323c98179140c4e",
-    "url": "/static/media/cueball.62cee232.png"
+    "url": "static/media/cueball.62cee232.png"
   },
   {
     "revision": "8dfa6cf47a7f9fbfefd4c21c709f19b8",
-    "url": "/static/media/target-view.8dfa6cf4.png"
+    "url": "static/media/target-view.8dfa6cf4.png"
   },
   {
     "revision": "83965708e46a3bd88ee59cb9e65f0f66",
-    "url": "/static/media/top-view.83965708.png"
+    "url": "static/media/top-view.83965708.png"
   }
 ]);
